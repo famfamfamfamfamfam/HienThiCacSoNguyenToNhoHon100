eb103391068fbf9262507493c76d17ae29f20f0f
BinhTran <fam.out16@gmail.com>
2024-08-12 21:47:36 +0700

    Add cac files cua bai tap hien thi cac so nguyen to nho hon 100

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SUDUNGCAULENHLAP
+{
+    class HienThiSoNguyenToNhoHon100
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 2; i <= 100; i++)
+            {
+                int solanchiahet = 0;
+                for (int j = 1; j <= Math.Sqrt(i); j++)
+                {
+                    if (i % j == 0)
+                    {
+                        solanchiahet++;
+                    }
+                }
+                if (solanchiahet == 1)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+        }
+    }
+}
